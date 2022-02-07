dc62db82478fb6b5d3cbad4843657259ffd1c633
Karina Białek <karinabialek2@gmail.com>
2022-02-07 16:27:39 +0100

    add item in cart view

--- js/script.js
+++ js/script.js
@@ -3,6 +3,12 @@ const cart = document.querySelector('.cart')
 const buttonPlus = document.querySelector('.amount__button-plus')
 const buttonMinus = document.querySelector('.amount__button-minus')
 const amountContainer = document.querySelector('.amount__number')
+const finalPrice = document.querySelector('.card__price-final')
+const amountContainerCard = document.querySelector('.card__price-amount')
+const addToCart = document.querySelector('.add__button')
+const emptyCard = document.querySelector('.cart__empty')
+const itemInCard = document.querySelector('.cart__item')
+const buttonDelete = document.querySelector('.card__item-delete')
 
 let amount = 0
 
@@ -23,6 +29,14 @@ buttonPlus.addEventListener(
         let newAmount = amount + 1
         amount = newAmount
         amountContainer.innerText = amount
+        amountContainerCard.innerText = amount
+
+        if (amount === 0) {
+            addToCart.disabled = true
+        } else {
+            addToCart.disabled = false
+            countFinalPrice()
+        }
     }
 )
 
@@ -32,10 +46,42 @@ buttonMinus.addEventListener(
         if (amount === 0) {
             amount = 0
             amountContainer.innerText = amount
+            addToCart.disabled = true
         } else {
             let newAmount = amount - 1
             amount = newAmount
             amountContainer.innerText = amount
+            amountContainerCard.innerText = amount
+            addToCart.disabled = false
+            countFinalPrice()
         }
+    }
+)
+
+addToCart.addEventListener(
+    'click',
+    () => {
+        emptyCard.style.display = 'none'
+        itemInCard.style.display = 'block'
+        countFinalPrice()
+
+    }
+
+)
+
+const countFinalPrice = function () {
+    let result = 125 * amount
+    finalPrice.innerHTML = '$' + result
+}
+
+
+buttonDelete.addEventListener(
+    'click',
+    () => {
+        emptyCard.style.display = 'block'
+        itemInCard.style.display = 'none'
+        amount = 0
+        amountContainer.innerText = amount
+        amountContainerCard.innerText = amount
     }
 )
